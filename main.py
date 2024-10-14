d874d169a42dfdabc4035b8c99f942ce75326840
shane <sm0862512@otc.edu>
2024-10-13 22:44:29 -0500

    works

--- main.py
+++ main.py
@@ -1,121 +1,89 @@
-import sqlite3
-import cv2 as cv
-import os
-import requests
-from tqdm import tqdm
-from multiprocessing import Pool
+import cv2
+import numpy as np
+import subprocess
+import time
 
+def is_contour_inside(contour1, contour2):
+    """Check if contour1 is inside contour2."""
+    point = tuple(float(coord) for coord in contour1[0][0])
+    return cv2.pointPolygonTest(contour2, point, False) > 0
 
-# def download_database():
-#     url = 'https://mtgjson.com/api/v5/AllPrintings.sqlite'
-#     local_filename = 'AllPrintings.sqlite'
-#     response = requests.get(url, stream=True)
-#     total_size = int(response.headers.get('content-length', 0))
-#     block_size = 8192  # 8 Kibibytes
-#     tqdm_bar = tqdm(total=total_size, unit='iB', unit_scale=True)
-#
-#     with open(local_filename, 'wb') as f:
-#         for chunk in response.iter_content(chunk_size=block_size):
-#             if chunk:
-#                 f.write(chunk)
-#                 tqdm_bar.update(len(chunk))
-#     tqdm_bar.close()
-#     print(f"Downloaded {local_filename}")
+# Try different camera indices
+for index in range(5):
+    cap = cv2.VideoCapture(index)
+    if cap.isOpened():
+        print(f"Camera opened successfully with index {index}")
+        break
+    cap.release()
+else:
+    raise IOError("Cannot open camera. Please check if the camera is connected and accessible.")
 
+# Set the frame width and height
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)  # Replace with your camera's max width
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)  # Replace with your camera's max height
 
-def query_database(name):
-    # Connect to the SQLite database
-    conn = sqlite3.connect('AllPrintings.sqlite')
-    # Create a cursor
-    c = conn.cursor()
-    # Execute the query
-    c.execute("SELECT * FROM cardidentifiers WHERE uuid=?", (name,))
-    # Fetch all the results
-    results = c.fetchall()
-    # Close the connection
-    conn.close()
-    return results
+# Initialize the timer
+start_time = None
+timer_threshold = 5  # Set the timer threshold to 5 seconds
 
+while True:
+    # Capture a frame
+    ret, img = cap.read()
 
-def calculate_good_matches(args):
-    filename, target_dir = args
-    # Load the query image
-    query_img = cv.imread('Magic-Card.png', cv.IMREAD_GRAYSCALE)
-    # Load the current image
-    current_img = cv.imread(os.path.join(target_dir, filename), cv.IMREAD_GRAYSCALE)
-    # Check if images are loaded correctly
-    if query_img is None or current_img is None:
-        return filename, 0, 0
-    # Resize the query image
-    query_img = cv.resize(query_img, (100, 100))
-    # Resize the current image
-    current_img = cv.resize(current_img, (100, 100))
-    # Convert images to grayscale if they are not
-    if len(query_img.shape) == 3:
-        query_img = cv.cvtColor(query_img, cv.COLOR_BGR2GRAY)
-    if len(current_img.shape) == 3:
-        current_img = cv.cvtColor(current_img, cv.COLOR_BGR2GRAY)
-    # Initiate SIFT detector
-    sift = cv.SIFT_create()
-    # Find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(query_img, None)
-    kp2, des2 = sift.detectAndCompute(current_img, None)
-    # Check if descriptors are computed correctly
-    if des1 is None or des2 is None:
-        return filename, 0, 0
-    # BFMatcher with default params
-    bf = cv.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
-    # Calculate total number of matches
-    total_matches = len(matches)
-    # Apply ratio test
-    good = []
-    for m, n in matches:
-        if m.distance < 0.75 * n.distance:
-            good.append([m])
-    # Calculate match percentage
-    match_percentage = (len(good) / total_matches) * 100
-    return filename, len(good), match_percentage
+    # Check if the frame is captured correctly
+    if not ret or img is None:
+        raise IOError("Cannot capture frame. Please check if the camera is working properly.")
 
+    # Apply the image processing steps
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (13,13), 0)
+    edges = cv2.Canny(blur, 50, 150)
+    kernel = np.ones((5,5), np.uint8)
+    dilated = cv2.dilate(edges, kernel, iterations=2)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-if __name__ == '__main__':
-    # Download the latest database file
-    # download_database()
-    # Directory with the photostorage images
-    target_dir = 'images/'
-    # Variables to keep track of the best match
-    best_match = None
-    max_good_matches = 0
-    best_match_percentage = 0
-    # Create a multiprocessing Pool
-    with Pool() as p:
-        # Loop over all images in the photostorage directory
-        for filename in tqdm(os.listdir(target_dir)):
-            # Process each image individually
-            filename, num_good_matches, match_percentage = p.apply(calculate_good_matches,
-                                                                   args=((filename, target_dir),))
-            # If this image is a better match than the current best match, update the best match
-            if num_good_matches > max_good_matches:
-                max_good_matches = num_good_matches
-                best_match = filename
-                best_match_percentage = match_percentage
-    # Print the best match
-    print(f"The best match is: {best_match}")
-    print(f"Match percentage: {best_match_percentage}%")
-    print(f"Number of good matches: {max_good_matches}")
-    # Load the query image
-    query_img = cv.imread('Magic-Card.png')
-    # Load the best match
-    best_match_img = cv.imread(os.path.join(target_dir, best_match))
-    # Display the images
-    name, extension = os.path.splitext(best_match)
-    results = query_database(name)
-    print(results[0][15])
-    apiurl = requests.get('https://api.scryfall.com/cards/' + results[0][15])
-    print(apiurl.json()['scryfall_uri'])
-    os.system(f'start {apiurl.json()['scryfall_uri']}')
-    query_img = cv.resize(query_img, (500, 500))
-    # cv.imshow('Query Image', query_img)
-    # cv.imshow('Best Match', best_match_img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    # Filter out contours that are inside other contours
+    outer_contours = [contour for i, contour in enumerate(contours) if not any(is_contour_inside(contour, other_contour) for j, other_contour in enumerate(contours) if i != j)]
+
+    if outer_contours:
+        # Find the largest contour
+        largest_contour = max(outer_contours, key=cv2.contourArea)
+
+        # Start the timer if it's not started
+        if start_time is None:
+            start_time = time.time()
+
+        # Check if the timer has reached the threshold
+        if time.time() - start_time >= timer_threshold:
+            # Use minimum area rectangle to approximate the contour
+            rect = cv2.minAreaRect(largest_contour)
+            box = cv2.boxPoints(rect)
+            box = np.intp(box)  # Use np.intp instead of np.int0 to avoid deprecation warning
+
+            # Draw a box around the card
+            cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
+
+            # Crop and save the image of the box
+            x, y, w, h = cv2.boundingRect(largest_contour)
+            cropped_img = img[y:y+h, x:x+w]
+            cv2.imwrite('Magic-Card.png', cropped_img)
+
+            # Run the command and exit
+            cv2.destroyAllWindows()
+            subprocess.run(["python", "photo.py"])
+            break
+    else:
+        # Reset the timer if the largest contour is not found
+        start_time = None
+
+    # Display the original frame
+    cv2.imshow("Original", img)
+    cv2.resizeWindow("Original", 1800, 1000)  # Resize the window to 640x480
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close all windows
+cap.release()
+cv2.destroyAllWindows()
